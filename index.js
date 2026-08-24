6278f35f1cb9d6b7acf9e8db0b591d500bd6c913
Ubuntu <ubuntu@ip-172-31-1-95.ec2.internal>
2026-08-24 05:48:54 +0000

    most updated indx.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello! This Node.js application is running successfully azam.");
+    res.send("Hii! This Node.js application is running successfully azam.");
 });
 
 app.get("/health", (req, res) => {
